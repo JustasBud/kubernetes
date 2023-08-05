1deafec078e874a376042d5571e56a8bd0bcd37a
Justas Bud <>
2023-08-05 22:49:16 +0100

    update

--- Post_Provision_Scripts/1_Setup_Kubectl_Azure.sh
+++ Post_Provision_Scripts/1_Setup_Kubectl_Azure.sh
@@ -1,14 +1,12 @@
 # connect kubectl to k8 aks:
 az login
 
-az aks get-credentials \
-  --resource-group xxxxxx-rg \
-  --name mycluster \
-  --subscription xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+az aks get-credentials --resource-group xxxxxx-rg --name mycluster --subscription xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 
 
 
-# get endpoints (ports) - test if kubectl is configured properly
+# get endpoints (ports) - test if kubectl is set up correctly. Run 1_Setup_Kubectl_<cloud>.sh if not set up. 
+# If connection timed out -  check firewall rules on k8 provider - whitelist IP for VM or set up VPC/Virtual Network
 kubectl get endpoints
 
 
