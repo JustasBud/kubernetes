d46b9ced20279f23ab2d7a5f75a08fc3f5090f00
Justas Bud <>
2023-08-08 21:31:05 +0100

    update

--- Provisioning_Scripts/Azure/AzureVM/Azure.sh
+++ Provisioning_Scripts/Azure/AzureVM/Azure.sh
@@ -63,4 +63,4 @@ sudo service xrdp restart
 
 # install browser for rdp sessions - does not come with one when xrdp is used:
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
