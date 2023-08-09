0ef758ba24878ad4fcd5108acd33b6fc6ef1798d
Justas Bud <>
2023-08-09 14:23:15 +0100

    update

--- Post_Provision_Scripts/K8Dashboard/ManualProvision_K8Dashboard.sh
+++ Post_Provision_Scripts/K8Dashboard/ManualProvision_K8Dashboard.sh
@@ -8,19 +8,29 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
 
-# Access kubernetes dashboard
-
-kubectl proxy
-
-
-
-kubectl apply -f dashboard-adminuser.yaml
+# Access kubernetes api server (will not work on remote even with --address 0.0.0.0 specified, as per docs (due to security), but to investigate further)
+# kubectl proxy
+# Access via API server directly, but for local access only:
+# http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
 
-http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
+# Create Service Account and ClusterRoleBinding
+kubectl apply -f https://raw.githubusercontent.com/JustasBud/kubernetes/main/Post_Provision_Scripts/K8Dashboard/dashboard-adminuser.yaml
 
+# Get admin-user token
 kubectl -n kubernetes-dashboard create token admin-user
+
+
+
+kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 8443:443 --address 0.0.0.0
+
+# Access DB via:
+https://external.ip.address:8443
+
+
+
+
 
 
 
